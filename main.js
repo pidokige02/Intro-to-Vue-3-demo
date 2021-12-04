089230d884ee3030fd354ff2edd1b166949cbb09
pidokige02 <pidokige0204@gmail.com>
2021-12-04 22:22:10 +0900

    L4-lesson

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            onSale: true,
+            inventory : 8,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
+
 
