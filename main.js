eb14b19017ec6479971cdd2c016d573c2319f396
pidokige02 <pidokige0204@gmail.com>
2021-12-04 23:45:51 +0900

    L6-lesson

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            onSale: true,
-            inventory : 8,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            inStock: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        // solution
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        // solution
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
+
 
 
