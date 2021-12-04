b8d4287498fa1d23b3dd7db88c730a7cfcc27f65
pidokige02 <pidokige0204@gmail.com>
2021-12-04 22:52:31 +0900

    L5-lesson

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            onSale: true,
-            inventory : 8,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            // solution
+            sizes: ['S', 'M', 'L', 'XL'],
+            // solution
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
