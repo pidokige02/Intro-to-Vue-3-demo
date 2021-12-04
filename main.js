812b118b8b116d3971b1bed8f33f4e4601307f28
pidokige02 <pidokige0204@gmail.com>
2021-12-05 07:06:47 +0900

    L7-lesson

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
             inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
@@ -16,18 +17,8 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        // solution
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        // solution
         updateImage(variantImage) {
             this.image = variantImage
         }
     }
 })
-
-
-
